0075c5c29a85d3f05f489d9adaf7343c2675d87a
Preetham <j.p.rao1327>
2024-02-13 10:19:30 +0530

    adding the header

---
+++ src/components/Header/index.js
@@ -0,0 +1,18 @@
+import './index.css';
+
+const Header=()=>{
+    return(
+        <header className="nav-header">
+            <a href="#" className="logo">Logo </a>
+            <nav className="navbar">
+                <a href="#" className="Logo">Home</a>
+                <a href="#" className="Logo">Features</a>
+                <a href="#" className="Logo">Pricing</a>
+                <a href="#" className="Logo">Services</a>
+                <a href="#" className="Logo">Contact Us</a>
+            </nav>
+        </header>
+    )
+}
+
+export default Header;
